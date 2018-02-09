9c210e16fc2a24cfc9ae848de33992df930641ff
Jurek Kamil <kamil.jurek@hemmersbach.com>
2018-02-09 08:54:05 +0100

    Some benchmarks to be rewritten in jmh

--- src/main/java/com/soze/klecs/TempMain.java
+++ src/main/java/com/soze/klecs/TempMain.java
@@ -20,54 +20,124 @@ public class TempMain {
     componentContainer.addComponent(1, Byte.valueOf("5"));
     componentContainer.addComponent(1, Long.valueOf("5"));
     componentContainer.addComponent(1, new LinkedList<>());
+    componentContainer.addComponent(1, new HashMap<>());
+    componentContainer.addComponent(1, Short.valueOf("5"));
 
     Node node = Node.of(
-      String.class, Integer.class, Object.class, ArrayList.class,
-      HashSet.class, Byte.class, Long.class, LinkedList.class
+        String.class, Integer.class, Object.class, ArrayList.class,
+        HashSet.class, Byte.class, Long.class, LinkedList.class,
+        HashMap.class, Short.class
     );
 
-    int iterations = 500000;
+    Node smallerNode = Node.of(String.class, LinkedList.class, Long.class);
+
+    int iterations = 50000;
 
     //1. get entire node
-    for(int i = 0; i < 100; i++) {
-      testNode(componentContainer, node, iterations);
+    int loops = 100;
+    long t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testNode(componentContainer, node, iterations);
     }
 
-    //1. get components one by one
-    for(int i = 0; i < 100; i++) {
-      testNotNode(componentContainer, node, iterations);
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testNode(componentContainer, node, iterations);
     }
+
+    System.out.println("Took " + (t / 1e9) + " s to get node with " + 10 + " elements " + iterations + " times.");
+
+    //2. get components one by one
+    for (int i = 0; i < loops; i++) {
+      t+= testNotNode(componentContainer, node, iterations);
+    }
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testNotNode(componentContainer, node, iterations);
+    }
+
+    System.out.println("Took " + (t / 1e9) + " s to get " + 10 + " elements one by one " + iterations + " times.");
+
+    //3. get node but remove components sometimes
+    for (int i = 0; i < loops; i++) {
+      t+= testRemove(componentContainer, node, iterations);
+    }
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testRemove(componentContainer, node, iterations);
+    }
+
+    System.out.println("Took " + (t / 1e9) + " s to get node with " + 10 + " elements " + iterations + " times (25% chance to remove one component).");
+
+    //4. get entire node
+    for (int i = 0; i < loops; i++) {
+      t+= testNode(componentContainer, smallerNode, iterations);
+    }
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testNode(componentContainer, smallerNode, iterations);
+    }
+
+    System.out.println("Took " + (t / 1e9) + " s to get node with " + 3 + " elements " + iterations + " times.");
+
+    //5. get components one by one
+    for (int i = 0; i < loops; i++) {
+      t += testNotNode(componentContainer, smallerNode, iterations);
+    }
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testNotNode(componentContainer, smallerNode, iterations);
+    }
+
+    System.out.println("Took " + (t / 1e9) + " s to get " + 3 + " elements one by one " + iterations + " times.");
+
+    //6. get node but remove components sometimes
+    for (int i = 0; i < loops; i++) {
+      t += testRemove(componentContainer, smallerNode, iterations);
+    }
+    t = 0;
+    for (int i = 0; i < loops; i++) {
+      t += testRemove(componentContainer, smallerNode, iterations);
+    }
+
+    System.out.println("Took " + (t / 1e9) + " s to get node with " + 3 + " elements " + iterations + " times (25% chance to remove one component).");
+
+
+
   }
 
-  private static void testNode(ComponentContainer componentContainer, Node node, int iterations) {
-    //1. get entire node
+  private static long testNode(ComponentContainer componentContainer, Node node, int iterations) {
     long t0 = System.nanoTime();
-//    bh = new ArrayList<>(iterations);
-    for(int i = 0; i < iterations; i++) {
+    for (int i = 0; i < iterations; i++) {
       consume(componentContainer.getNodeComponents(1, node));
     }
 
-    double time = (System.nanoTime() - t0) / 1e9;
-    System.out.println("Took " + time + " s to get node with " + 8 + " elements " + iterations + " times.");
+    return System.nanoTime() - t0;
   }
 
-  private static void testNotNode(ComponentContainer componentContainer, Node node, int iterations) {
-//1. get components one by one
+  private static long testNotNode(ComponentContainer componentContainer, Node node, int iterations) {
     long t0 = System.nanoTime();
-//    bh = new ArrayList<>(iterations);
-    for(int i = 0; i < iterations; i++) {
-      consume(componentContainer.getComponent(1, String.class));
-      consume(componentContainer.getComponent(1, Integer.class));
-      consume(componentContainer.getComponent(1, Object.class));
-      consume(componentContainer.getComponent(1, ArrayList.class));
-      consume(componentContainer.getComponent(1, HashSet.class));
-      consume(componentContainer.getComponent(1, Byte.class));
-      consume(componentContainer.getComponent(1, Long.class));
-      consume(componentContainer.getComponent(1, LinkedList.class));
+    for (int i = 0; i < iterations; i++) {
+      node.getComponentClasses().forEach(clazz -> consume(componentContainer.getComponent(1, clazz)));
     }
 
-    double time = (System.nanoTime() - t0) / 1e9;
-    System.out.println("Took " + time + " s to get " + 8 + " components " + iterations + " times.");
+    return System.nanoTime() - t0;
+  }
+
+  private static long testRemove(ComponentContainer componentContainer, Node node, int iterations) {
+    long t0 = System.nanoTime();
+    for (int i = 0; i < iterations; i++) {
+      boolean mess = Math.random() < 2;
+      if (mess) {
+        componentContainer.removeComponent(1, String.class);
+      }
+      consume(componentContainer.getNodeComponents(1, node));
+      if (mess) {
+        componentContainer.addComponent(1, "5");
+      }
+    }
+
+    return System.nanoTime() - t0;
   }
 
   private static void consume(Object obj) {
